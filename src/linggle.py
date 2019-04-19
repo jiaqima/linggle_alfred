31fd4ff5e3c1e680969a1ac9175006bd843bf5a4
jiaqima-mac <mjq44bruce@163.com>
2019-04-19 14:52:14 -0400

    better format of the code

--- src/linggle.py
+++ src/linggle.py
@@ -7,7 +7,6 @@ from workflow import Workflow
 
 import urllib2
 import json
-import re
 
 
 def main(wf):
@@ -16,26 +15,31 @@ def main(wf):
     args = parser.parse_args(wf.args)
     # itemlist = [v for v in args.query.strip().split(' ') if v]
     # token='%20'.join(itemlist)
-    token=args.query.strip().replace(' ','%20').replace('?','%3F').replace('/','%2F')
-    page = urllib2.urlopen(u'http://www.linggle.com/query/%s' %token,timeout=1000)
-    if page.getcode()==200:
+    token = args.query.strip().replace(' ', '%20').replace('?', '%3F').replace(
+        '/', '%2F')
+    page = urllib2.urlopen(
+        u'http://www.linggle.com/query/%s' % token, timeout=1000)
+    if page.getcode() == 200:
         # wf.add_item('Yes')
         # wf.send_feedback()
-        result=json.loads(page.read())
-        
+        result = json.loads(page.read())
+
         if not result:
             wf.add_item('No results')
             wf.send_feedback()
         else:
             for item in result:
-                wf.add_item(title=' '.join(item['phrase']).replace('<strong>','').replace('</strong>',''),subtitle=item['percent']+', '+item['count_str'])
+                wf.add_item(
+                    title=' '.join(item['phrase']).replace(
+                        '<strong>', '').replace('</strong>', ''),
+                    subtitle=item['percent'] + ', ' + item['count_str'])
             wf.send_feedback()
     else:
         wf.add_item('404')
         wf.send_feedback()
     return 0
-    
-if __name__==u"__main__":
-    wf=Workflow()
+
+
+if __name__ == u"__main__":
+    wf = Workflow()
     sys.exit(wf.run(main))
-    
